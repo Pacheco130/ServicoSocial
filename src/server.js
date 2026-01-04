f79e0c1233e8a201e6b47fabcde38f5117049e3b
Pacheco130 <juan-15210075@umb.edu.mx>
2026-01-03 19:32:29 -0600

    Peticion de cambio Diciembre

--- src/server.js
+++ src/server.js
@@ -628,7 +628,7 @@ app.use('/api', alumnoRoutes);
 // Modificar la configuración de rutas estáticas (reemplazar las existentes)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get(/.*/, (req, res) => {
+    app.get(/^\/(?!api).*/, (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 } else {
